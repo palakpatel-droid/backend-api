e6438d2b15040978632725bb8929f13f977685e2
palakpatel-droid <palak.patel@maction.com>
2026-01-07 15:03:50 +0530

    Add error simulation log to root route

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+  console.error("ALERT TEST: backend-api error simulation");
   res.send("Backend is running 🚀");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
